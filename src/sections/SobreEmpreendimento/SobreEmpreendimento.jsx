40498295d22d6791bd8a4dab1262dc454e04cf1f
Gilberto Santos <gfernandesdc@gmail.com>
2026-07-16 10:47:29 -0300

    Atualiza alguns textos

--- src/sections/SobreEmpreendimento/SobreEmpreendimento.jsx
+++ src/sections/SobreEmpreendimento/SobreEmpreendimento.jsx
@@ -14,22 +14,26 @@ function SobreEmpreendimento() {
         </h2>
 
         <p>
-          O Caminhos da Guanabara foi planejado para
+          O Empreendimento Caminhos da Guanabara foi planejado para
           quem busca praticidade, conforto e qualidade
           de vida em uma das regiões mais tradicionais
           e bem conectadas de Niterói.
-
+        </p >
+   
+        <p >
+          Os pontos mais importantes da cidade, com fácil acesso a serviços,
+          comércio, educação e transporte, sem abrir mão
+          da tranquilidade de viver em um condomínio
+          moderno e completo.
+        </p>
+         
+        <p >
           Com apartamentos inteligentes, áreas de lazer
           completas e espaços pensados para o dia a dia,
           o empreendimento reúne tudo o que sua família
           precisa para viver com mais comodidade,
           mobilidade e bem-estar.
 
-          Aqui você estará perto de importantes pontos
-          da cidade, com fácil acesso a serviços,
-          comércio, educação e transporte, sem abrir mão
-          da tranquilidade de viver em um condomínio
-          moderno e completo.
         </p>
       </div>
     </section>
